a70f1bda96efebbbffb015d373c38bca30bf8c82
Simeon <simeonpopov94@gmail.com>
2023-01-13 05:07:14 +0200

    Refactored project structure. Created register back-end. Connected successfully to DB. Created first account in DB. Created DB table for registered users. Successfully created and used AJAX. Created validation for all fields in Registration form. !Removed require from fields in index.php. Error message front-end with custom message from back-end created. Image storing into subFile with name images ready. Images storing with name time+name ready.

--- index.php
+++ index.php
@@ -13,30 +13,30 @@
         <section class="form signup">
 
             <header>Realtime Chat App</header>
-            <form action="#">
-                <div class="error-message">This is error message!</div>
+            <form enctype="multipart/form-data">
+                <div class="error-message"></div>
                 <div class="name-details">
                     <div class="field input">
                         <label for="first_name">First Name</label>
-                        <input id="first_name" type="text" placeholder="First Name">
+                        <input id="first_name" type="text" name="first_name" placeholder="First Name">
                     </div>
                     <div class="field input">
                         <label for="last_name">Last Name</label>
-                        <input id="last_name" type="text" placeholder="Last Name">
+                        <input id="last_name" type="text" name="last_name" placeholder="Last Name">
                     </div>
                 </div>
                 <div class="field input">
                     <label for="email_address">Email Address</label>
-                    <input id="email_address" type="text" placeholder="Enter your email">
+                    <input id="email_address" type="text" name="email_address" placeholder="Enter your email">
                 </div>
                 <div class="field input">
                     <label for="password">Password</label>
-                    <input id="password" type="password" placeholder="Enter new password">
+                    <input id="password" type="password" name="password" placeholder="Enter new password">
                     <i class="fas fa-eye"></i>
                 </div>
                 <div class="field image">
-                    <label for="image_input">Select image</label>
-                    <input id="image_input" type="file">
+                    <label>Select image</label>
+                    <input  type="file" name="image">
                 </div>
                 <div class="field button">
                     <input type="submit" value="Continue to Chat">
@@ -51,6 +51,7 @@
         </section>
     </div>
 
-    <script src="../javascript/pass-show-hide.js"></script>
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/signup.js"></script>
 </body>
 </html>
---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+    $db_ip = 'localhost:3306';
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "social_platform";
+    $conn = new mysqli($db_ip, $db_username, $db_password, $db_name);
